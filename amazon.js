3e98752cffa2a6632e13dffc7a8d19c4ef444d83
pratik5045 <162993462+pratik5045@users.noreply.github.com>
2024-06-08 16:38:25 +0530

    Add files via upload

---
+++ amazon.js
@@ -0,0 +1,10 @@
+var swp = new Swiper(".ms", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay:true,
+    navigation: {
+      nextEl: ".sbn",
+      prevEl: ".sbp",
+    },
+  });
